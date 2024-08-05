91fd6aeca0800a97bcb39928f316ff0bd5a2a13a
armandorucas <chamoy@gmail.com>
2024-08-05 10:19:28 -0500

    se finalizo el trabajo

--- MultiplicarNumeros/src/main/java/com/mycompany/multiplicarnumeros/MultiplicarNumeros.java
+++ MultiplicarNumeros/src/main/java/com/mycompany/multiplicarnumeros/MultiplicarNumeros.java
@@ -4,13 +4,19 @@
 
 package com.mycompany.multiplicarnumeros;
 
-/**
- *
- * @author Usuario
- */
+import java.util.Scanner;
+
 public class MultiplicarNumeros {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner lector= new Scanner (System.in);
+        System.out.println("Ingrese primer numero:");
+        int n1= lector.nextInt();
+        System.out.println("Ingrese segundo numero:");
+        int n2= lector.nextInt();
+        int r =(n1*n2); 
+        System.out.println("El resultado es:" + r);
+        
+                
     }
 }
